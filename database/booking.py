38d82ab9c7107054c389dd936ef5ee5f182a0806
eg-mart <marteg71@gmail.com>
2022-05-05 01:49:21 +0400

    Completed booking model

--- database/booking.py
+++ database/booking.py
@@ -1,9 +1,10 @@
 import sqlalchemy
 from sqlalchemy.orm import relationship
 from database.db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 
 
-class Booking(SqlAlchemyBase):
+class Booking(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'bookings'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -11,3 +12,6 @@ class Booking(SqlAlchemyBase):
     user_id = Column(sqlalchemy.Integer, 
                      sqlalchemy.ForeignKey('users.id'))
     user = relationship("User", back_populates="bookings")
+    tables = sqlalchemy.Column(sqlalchemy.String)
+    datetime = sqlalchemy.Column(sqlalchemy.DateTime)
+    duration = sqlalchemy.Column(sqlalchemy.Integer)
