f086a7ed434aff941d049bb10751e9d06edea1bb
eg-mart <marteg71@gmail.com>
2022-05-06 01:26:31 +0400

    Finished booking api

--- resources/bookings_resources.py
+++ resources/bookings_resources.py
@@ -2,6 +2,7 @@ from flask_restful import reqparse, abort, Resource
 from database.db_session import create_session
 from database.booking import Booking
 from flask import jsonify
+from datetime import datetime as dt
 
 
 def abort_if_not_found(booking_id):
@@ -13,7 +14,14 @@ def abort_if_not_found(booking_id):
             return abort(404, message=f"Booking {booking_id} not found")
 
 
-class BookingsResource(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('tables', required=True)
+parser.add_argument('datetime', required=True)
+parser.add_argument('duration', required=True, type=int)
+parser.add_argument('user_id', required=True, type=int)
+
+
+class BookingResource(Resource):
     def get(self, booking_id):
         abort_if_not_found(booking_id)
         session = create_session()
@@ -22,6 +30,22 @@ class BookingsResource(Resource):
             booking = session.query(Booking).get(booking_id)
             return jsonify({'booking': booking.to_dict(
                 only=('user_id', 'tables', 'datetime', 'duration'))})
+
+    def put(self, booking_id):
+        abort_if_not_found(booking_id)
+        args = parser.parse_args()
+        session = create_session()
+
+        datetime = dt.strptime(args['datetime'], "%a, %d %b %Y %X GMT")
+
+        with session.begin():
+            booking = session.query(Booking).get(booking_id)
+            booking.user_id = args['user_id']
+            booking.duration = args['duration']
+            booking.tables = args['tables']
+            booking.datetime = datetime
+        
+        return jsonify({'success': 'OK'})
     
     def delete(self, booking_id):
         abort_if_not_found(booking_id)
@@ -30,4 +54,30 @@ class BookingsResource(Resource):
         with session.begin():
             booking = session.query(Booking).get(booking_id)
             session.delete(booking)
+        return jsonify({'success': 'OK'})
+
+
+class BookingListResource(Resource):
+    def get():
+        session = create_session()
+        with session.begin():
+            bookings = session.query(Booking).all()
+            return jsonify({'bookings':[item.to_dict(
+                only=('user_id', 'tables', 'datetime', 'duration')) for item in bookings]})
+    
+    def post():
+        session = create_session()
+        args = parser.parse_args()
+
+        datetime = dt.strptime(args['datetime'], "%a, %d %b %Y %X GMT")
+
+        with session.begin():
+            booking = Booking(
+                user_id=args['user_id'],
+                duration=args['duration'],
+                datetime=datetime,
+                tables=args['tables']
+            )
+            session.add(booking)
+
         return jsonify({'success': 'OK'})
