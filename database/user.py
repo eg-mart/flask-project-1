6bebcfa30f6f0124d35e51167e0739d3a12e6b41
eg-mart <marteg71@gmail.com>
2022-05-10 00:33:21 +0400

    Completed User model

--- database/user.py
+++ database/user.py
@@ -10,6 +10,15 @@ class User(SqlAlchemyBase, UserMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    phone = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True)
     bookings = relationship("Booking", back_populates="user")
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    is_admin = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
