fc76ad5a2599460da736f57487dace17c43a534b
eg-mart <marteg71@gmail.com>
2022-05-13 17:01:30 +0400

    Fixed required validators

--- forms/add_booking.py
+++ forms/add_booking.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import widgets
 from wtforms import SelectMultipleField, DateField, StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, NumberRange, InputRequired
+from wtforms.validators import InputRequired, NumberRange, InputRequired
 import re
 
 
 class BookingForm(FlaskForm):
     tables = SelectMultipleField('Столики', choices=[i for i in range(1, 7)], coerce=int,
-                                 option_widget=widgets.CheckboxInput())
-    date = DateField('Дата брони')
-    start_time = IntegerField(validators=[DataRequired(), NumberRange(0, 23)])
-    end_time = IntegerField(validators=[DataRequired(), NumberRange(0, 23)])
+                                 option_widget=widgets.CheckboxInput(), validators=[InputRequired()])
+    date = DateField('Дата брони', validators=[InputRequired()])
+    start_time = IntegerField(validators=[InputRequired(), NumberRange(0, 23)])
+    end_time = IntegerField(validators=[InputRequired(), NumberRange(0, 23)])
     submit = SubmitField('Забронировать')
 
     def validate_time(form, field):
