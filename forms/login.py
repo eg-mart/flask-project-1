3949471cb2cc41e0a84e79f3cd7b302c842ec498
eg-mart <marteg71@gmail.com>
2022-05-13 13:29:36 +0400

    Added phone validation

--- forms/login.py
+++ forms/login.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+import re
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+    def validate_phone(form, field):
+        text = field.data
+        if text.startswith('8'):
+            text = text.replace('8', '+7', 1)
+        text = text.replace('(', '')
+        text = text.replace(')', '')
+        text = text.replace('-', '')
+        text = text.replace(' ', '')
+        if not re.search('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5}$', text):
+            raise ValidationError("Invalid input syntax")
+        form.phone = text
